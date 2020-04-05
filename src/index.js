6449288b00d10f3764645cd2d44424df271bd053
bongomin <candanobongomindaniel@gmail.com>
2020-04-05 10:18:38 +0300

    feat(Redirecting If user is Authenticated):
    - changed the Root stateless funtion to a stete class.
    - created RootWithAuth component
    
    - redirects when user authenticated

--- src/index.js
+++ src/index.js
@@ -1,26 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import * as serviceWorker from './serviceWorker';
+// import * as serviceWorker from './serviceWorker';
 import Login from './components/Auth/Login';
 import Signup from './components/Auth/Signup';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-
+import { BrowserRouter as Router, Switch, Route, withRouter } from 'react-router-dom';
+import firebase from './firebase';
 import 'semantic-ui-css/semantic.min.css';
 
 
-const Root = () => (
-  <Router>
-    <Switch>
-      <Route exact path="/" component={App} />
-      <Route exact path="/login" component={Login} />
-      <Route exact path="/register" component={Signup} />
-    </Switch>
-  </Router>
+class Root extends Component {
+  componentDidMount() {
+    firebase
+      .auth()
+      .onAuthStateChanged(user => {
+        if (user) {
+          this.props.history.push('/');
+        }
+      })
+  }
+  render() {
+    return (
 
-)
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Signup} />
+      </Switch>
 
-ReactDOM.render(<Root />, document.getElementById('root')
+
+    );
+  }
+}
+
+// handling history
+const RootWithAuth = withRouter(Root);
+
+ReactDOM.render(<Router>
+  <RootWithAuth />,
+</Router>, document.getElementById('root')
 );
 
-serviceWorker.unregister();
+// serviceWorker();
+
+
