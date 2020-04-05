c866348c8beba554157b8190d3d9564ed9786b2a
bongomin <candanobongomindaniel@gmail.com>
2020-04-05 19:43:30 +0300

    feat(channels component ):
    - created the Channels Component and imported it into the sidePanell component

---
+++ src/components/SidePanel/Channels.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import { Menu, Icon } from 'semantic-ui-react';
+
+
+class Channels extends Component {
+   state = {
+      channels: []
+   }
+   render() {
+      const { channels } = this.state
+      return (
+         <Menu.Menu style={{ paddingBottom: "2em" }}>
+            <Menu.Item>
+               <span>
+                  <Icon name="exchange" />CHANNELS
+               </span>
+               ({channels.length}) <Icon name="add" />
+            </Menu.Item>
+
+         </Menu.Menu>
+         // channels
+
+
+      )
+   }
+}
+
+export default Channels
