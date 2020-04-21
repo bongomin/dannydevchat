c5942f39c9bd63f2e0f1fb03d422aa6888116b91
bongomin <candanobongomindaniel@gmail.com>
2020-04-21 11:39:31 +0300

    Feat(Adding Channle Title:
    - added Channel Tiltle which a user is on
    
    - added userCounts in A channel

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -12,7 +12,8 @@ class Messages extends React.Component {
     messages: [],
     messagesLoading: true,
     channel: this.props.currentChannel,
-    user: this.props.currentUser
+    user: this.props.currentUser,
+    numUniqueUsers: ''
   };
 
   componentDidMount() {
@@ -35,8 +36,24 @@ class Messages extends React.Component {
         messages: loadedMessages,
         messagesLoading: false
       });
+      this.countUniqueUsers(loadedMessages);
     });
   };
+
+  countUniqueUsers = messages => {
+    const uniqueUsers = messages.reduce((acc, message) => {
+      if (!acc.includes(message.user.name)) {
+        acc.push(message.user.name);
+      }
+      return acc;
+
+    }, []);
+    // check number of users
+    const plural = uniqueUsers.length > 1 || uniqueUsers.length === 0;
+
+    const numUniqueUsers = `${uniqueUsers.length} user${plural ? 's' : ''}`;
+    this.setState({ numUniqueUsers })
+  }
 
   displayMessages = messages =>
     messages.length > 0 &&
@@ -48,12 +65,17 @@ class Messages extends React.Component {
       />
     ));
 
+  displayChannelName = channel => channel ? `# ${channel.name}` : '';
+
   render() {
-    const { messagesRef, messages, channel, user } = this.state;
+    const { numUniqueUsers, messagesRef, messages, channel, user } = this.state;
 
     return (
       <React.Fragment>
-        <MessagesHeader />
+        <MessagesHeader
+          channelName={this.displayChannelName(channel)}
+          numUniqueUsers={numUniqueUsers}
+        />
 
         <Segment>
           <Comment.Group className="messages">
--- src/components/Messages/MessagesHeader.js
+++ src/components/Messages/MessagesHeader.js
@@ -3,15 +3,20 @@ import { Header, Segment, Input, Icon } from "semantic-ui-react";
 
 class MessagesHeader extends React.Component {
   render() {
+
+    const { channelName, numUniqueUsers } = this.props;
+
+
     return (
       <Segment clearing>
         {/* Channel Title */}
         <Header fluid="true" as="h2" floated="left" style={{ marginBottom: 0 }}>
           <span>
-            Channel
+            {/* channel name here */}
+            {channelName}
             <Icon name={"star outline"} color="black" />
           </span>
-          <Header.Subheader>2 Users</Header.Subheader>
+          <Header.Subheader>{numUniqueUsers}</Header.Subheader>
         </Header>
 
         {/* Channel Search Input */}
